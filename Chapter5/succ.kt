bd91e98582b57e98143c1f64169153f73ab98a09
aq17 <aqneco@gmail.com>
2019-02-03 10:29:54 +0900

    square,hello,max関数の定義

--- Chapter5/succ.kt
+++ Chapter5/succ.kt
@@ -3,6 +3,15 @@ package sample
 fun main(args:Array<String>){
   val result = succ(31) //resultは型推論によって値を受け取る
   println(result)
+  println(square(7))
+  println(hello("Momiyama"))
+  println(max(12,13))
 }
 
-fun succ(i:Int): Int = i + 1 
+fun succ(i:Int): Int = i + 1
+
+fun square(i:Int): Int = i * i
+
+fun hello(name:String): String = "Hello, $name!"
+
+fun max(a:Int,b:Int): Int = if(b <= a) a else b
