553ca3db40ee00a79702e7032f1f9ea244c16b22
aq17 <aqneco@gmail.com>
2019-12-12 19:29:50 +0900

    fix

--- Atcoder/Atcoder/ABC147/c.cpp
+++ Atcoder/Atcoder/ABC147/c.cpp
@@ -1,12 +1,21 @@
 #include <bits/stdc++.h>
 using namespace std;
 
+int counter(int x) {
+  if (x == 0) {
+    return 0;
+  } else {
+    return counter(x >> 1) + (x & 1);
+  }
+}
+
 int main() {
   int n;
   cin >> n;
   int a[20];
   int x[20][20];
   int y[20][20];
+  int ans = 0;
 
   for (int i = 0; i < n; i++) {
     cin >> a[i];
@@ -16,7 +25,21 @@ int main() {
   }
 
   for (int bit = 0; bit < (1<<n); bit++) {
-    bool hokotate = true;
-    
+    bool ok = true; //矛盾したらfalse
+    for (int i = 0; i < n; i++) {
+      if (!(bit & (1 << i))){
+        continue;
+      }
+      for (int j = 0; j < a[i]; j++) {
+        if (((bit >> (x[i][j])) & 1) ^ y[i][j]) {
+          ok = false;
+        }
+      }
+    }
+    if (ok) {
+      ans = max(ans, counter(bit));
+    }
   }
+  cout << ans << endl;
+  return 0;
 }
