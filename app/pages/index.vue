07881ee16eae40ceb8ae8ac8519c6dd82b1fcb60
momeemt <momiimt@gmail.com>
2020-05-02 15:49:30 +0900

    ✨ ログイン・新規登録postの実装

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -15,7 +15,7 @@
           </el-checkbox>
         </div>
         <div class="text-right">
-          <el-button type="primary">
+          <el-button @click="handleClickSubmit" type="primary">
             {{ buttonText }}
           </el-button>
         </div>
@@ -25,19 +25,68 @@
 </template>
 
 <script>
+// eslint-disable-next-line standard/object-curly-even-spacing
+import { /* mapGetters, */ mapActions } from 'vuex'
 export default {
   computed: {
     buttonText () {
       return this.isCreateMode ? '新規登録' : 'ログイン'
     }
   },
-  asyncData () {
+  asyncData ({ redirect, store }) {
+    if (store.getters.user) {
+      redirect('/posts')
+    }
     return {
       isCreateMode: false,
       formData: {
         id: ''
       }
     }
+  },
+  methods: {
+    async handleClickSubmit () {
+      if (this.isCreateMode) {
+        try {
+          await this.register({ ...this.formData })
+          this.$notify({
+            type: 'success',
+            title: 'アカウント作成完了',
+            message: `${this.formData.id} として登録しました`,
+            position: 'bottom-right',
+            duration: 5000
+          })
+          await this.$router.push('/words')
+        } catch (_) {
+          this.$notify.error({
+            title: 'アカウント作成失敗',
+            message: '既に登録されているか、不正なユーザーIDです',
+            position: 'bottom-right',
+            duration: 5000
+          })
+        }
+      } else {
+        try {
+          await this.login({ ...this.formData })
+          this.$notify({
+            type: 'success',
+            title: 'ログイン成功',
+            message: `${this.formData.id}としてログインしました`,
+            position: 'button-right',
+            duration: 5000
+          })
+          await this.$router.push('/words/')
+        } catch (e) {
+          this.$notify.error({
+            title: 'ログイン失敗',
+            message: '不正なユーザーIDです',
+            position: 'bottom-right',
+            duration: 5000
+          })
+        }
+      }
+    },
+    ...mapActions(['login', 'register'])
   }
 }
 </script>
