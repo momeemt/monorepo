af7ced35d23b8de2f1796bfb6d2e6107f76a0e20
Momeemt <momiimt@gmail.com>
2020-07-24 13:23:23 +0900

    ✨Retrofitの実装

--- QiitaClient/app/src/main/java/com/sample/MainActivity.kt
+++ QiitaClient/app/src/main/java/com/sample/MainActivity.kt
@@ -3,9 +3,15 @@ package com.sample
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.ListView
+import com.google.gson.FieldNamingPolicy
+import com.google.gson.GsonBuilder
+import com.sample.client.ArticleClient
 import com.sample.model.Article
 import com.sample.model.User
 import com.sample.view.ArticleView
+import retrofit2.Retrofit
+import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory
+import retrofit2.converter.gson.GsonConverterFactory
 
 class MainActivity : AppCompatActivity() {
 
@@ -26,6 +32,19 @@ class MainActivity : AppCompatActivity() {
             val article = listAdapter.articles[position]
             ArticleActivity.intent(this, article).let { startActivity(it) }
         }
+
+        val gson = GsonBuilder()
+            .setFieldNamingPolicy(FieldNamingPolicy.LOWER_CASE_WITH_UNDERSCORES)
+            .create()
+
+        val retrofit = Retrofit
+            .Builder()
+            .baseUrl("https:qiita.com")
+            .addConverterFactory(GsonConverterFactory.create(gson))
+            .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
+            .build()
+
+        val articleClient = retrofit.create(ArticleClient::class.java)
     }
 
     private fun dummyArticle(title: String, userName: String): Article =
