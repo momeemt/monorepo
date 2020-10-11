06e4faf1e459908ea7b58e8dffaf58e18ee8c531
momeemt <momiimt@gmail.com>
2020-10-11 23:08:52 +0900

    🎉  Init Ammonite!

---
+++ lib/ammonite.rb
@@ -0,0 +1,7 @@
+require "ammonite/ppm"
+require "ammonite/version"
+
+module Ammonite
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/ammonite/ppm.rb
@@ -0,0 +1,49 @@
+module Ammonite
+  class PPM
+    def initialize(x, y)
+      @binary = Array.new(x).map {
+        Array.new(y).map {
+          Array.new(3, 255)
+        }
+      }
+      @pixel_x = x
+      @pixel_y = y
+      self
+    end
+
+    def fill_all(red, green, blue)
+      (0...@pixel_y).each { |y|
+        (0...@pixel_x).each { |x|
+          @binary[y][x][0] = red
+          @binary[y][x][1] = green
+          @binary[y][x][2] = blue
+        }
+      }
+      self
+    end
+
+    def fill(red, green, blue, pos1, pos2)
+      (pos1.y...pos2.y).each do |y|
+        (pos1.x...pos2.x).each do |x|
+          @binary[y][x][0] = red
+          @binary[y][x][1] = green
+          @binary[y][x][2] = blue
+        end
+      end
+      self
+    end
+
+    def save(name)
+      File.open("#{name}.ppm", "w") do |file|
+        file.write "P6\n"
+        file.write "#{@pixel_x} #{@pixel_y}\n"
+        file.write "255\n"
+        (0...@pixel_y).each { |y|
+          (0...@pixel_x).each { |x|
+            file.write @binary[y][x][0].chr + @binary[y][x][1].chr + @binary[y][x][2].chr
+          }
+        }
+      end
+    end
+  end
+end
