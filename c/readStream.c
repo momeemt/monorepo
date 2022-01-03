854e505b712569fa85c09a7649edc21bf7e31f8f
Mutsuha Asada <momiimt@gmail.com>
2022-01-03 21:15:38 +0900

    👍 execise 1-12. separating words

--- c/readStream.c
+++ c/readStream.c
@@ -1,27 +1,11 @@
 #include <stdio.h>
 
 int main () {
-  int c, whitespace, tab, newline;
+  int c;
 
-  whitespace = 0;
-  tab = 0;
-  newline = 0;
   while ((c = getchar()) != EOF) {
-    if (c == ' ') {
-      while (c == ' ') {
-        c = getchar();
-      }
-      putchar(' ');
-    }
-    if (c == '\t') {
-      putchar('\\');
-      putchar('t');
-    } else if (c == '\b') {
-      putchar('\\');
-      putchar('b');
-    } else if (c == '\\') {
-      putchar('\\');
-      putchar('\\');
+    if (c == ' ' || c == '\n' || c == '\t') {
+      putchar('\n');
     } else {
       putchar(c);
     }
