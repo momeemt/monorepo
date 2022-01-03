2c48cb68f9e38290a728d35e1ac793b0fc8e740b
Mutsuha Asada <momiimt@gmail.com>
2022-01-03 20:37:40 +0900

    👍 count whitespace, tab and newline

--- c/readStream.c
+++ c/readStream.c
@@ -1,11 +1,20 @@
 #include <stdio.h>
 
 int main () {
-  int c;
+  int c, whitespace, tab, newline;
 
-  while ((c = getchar()) != EOF) {
-    putchar(c);
-  }
-
-  printf("%d", c);
+  whitespace = 0;
+  tab = 0;
+  newline = 0;
+  while ((c = getchar()) != EOF)
+    if (c == ' ') {
+      ++whitespace;
+    } else if (c == '\t') {
+      ++tab;
+    } else if (c == '\n') {
+      ++newline;
+    }
+  printf("空白: %d\n", whitespace);
+  printf("タブ: %d\n", tab);
+  printf("改行: %d\n", newline);
 }
