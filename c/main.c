77f4011a26cde1ba3b8aa39bbf1897c20ffdc126
Mutsuha Asada <momiimt@gmail.com>
2022-01-03 20:14:09 +0900

    👍 execise 1-4. fahr to celsius

--- c/main.c
+++ c/main.c
@@ -1,5 +1,13 @@
 #include <stdio.h>
 
+float fahrToCelsius (float degree) {
+  return (5.0 / 9.0) * (degree - 32.0);
+}
+
+float celsiusToFahr (float degree) {
+  return degree * (9.0 / 5.0) + 32.0;
+}
+
 int main () {
   float fahr, celsius;
   int lower, upper, step;
@@ -8,12 +16,12 @@ int main () {
   upper = 300;
   step = 20;
 
-  fahr = lower;
-  printf("%5s\tcelsius\n", "fahr");
+  celsius = lower;
+  printf("%7s\t%7s\n", "celsius", "fahr");
   printf("----------------\n");
   while (fahr <= upper) {
-    celsius = (5.0 / 9.0) * (fahr - 32.0);
-    printf("%5.0f\t%7.1f\n", fahr, celsius);
-    fahr = fahr + step;
+    fahr = celsiusToFahr(celsius);
+    printf("%7.0f\t%7.1f\n", celsius, fahr);
+    celsius += step;
   }
 }
