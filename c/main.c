1ce27428cd1606054c2b14c560796380e8724897
Mutsuha Asada <momiimt@gmail.com>
2022-01-03 20:20:30 +0900

    👍 define symbolic constant

--- c/main.c
+++ c/main.c
@@ -1,5 +1,9 @@
 #include <stdio.h>
 
+#define LOWER 0
+#define UPPER 300
+#define STEP 20
+
 float fahrToCelsius (float degree) {
   return (5.0 / 9.0) * (degree - 32.0);
 }
@@ -9,15 +13,9 @@ float celsiusToFahr (float degree) {
 }
 
 int main () {
-  int lower, upper, step;
-
-  lower = 0;
-  upper = 300;
-  step = 20;
-
   printf("%5s\t%7s\n", "fahr", "celsius");
   printf("----------------\n");
-  for (float fahr = upper; fahr >= lower; fahr -= step) {
+  for (float fahr = UPPER; fahr >= LOWER; fahr -= STEP) {
     float celsius = fahrToCelsius(fahr);
     printf("%5.0f\t%7.1f\n", fahr, celsius);
   }
