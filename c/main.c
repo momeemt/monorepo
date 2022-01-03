98950758f229e5c13d63fac6e9fccaa78e299c95
Mutsuha Asada <momiimt@gmail.com>
2022-01-03 20:01:03 +0900

    👍 convert between Celsius and Fahrenheit

--- c/main.c
+++ c/main.c
@@ -1,8 +1,17 @@
 #include <stdio.h>
 
 int main () {
-  printf("hello, world\n");
-  printf("hello, world\t\n");
-  printf("hello, world\b \n");
-  printf("hello, world\"\n");
+  int fahr, celsius;
+  int lower, upper, step;
+
+  lower = 0;
+  upper = 300;
+  step = 20;
+
+  fahr = lower;
+  while (fahr <= upper) {
+    celsius = 5 * (fahr - 32) / 9;
+    printf("%d\t%d\n", fahr, celsius);
+    fahr = fahr + step;
+  }
 }
