a77b976a12b2a6494978334b23c06ad9b3693e14
Mutsuha Asada <momiimt@gmail.com>
2022-07-25 04:04:06 +0900

    🎉 install: neovim

--- install.sh
+++ install.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 
-brew bundle install
-
 unlink ~/.zshrc
 ln -s ${PWD}/src/.zshrc ~/.zshrc
 
 unlink ~/.zprofile
 ln -s ${PWD}/src/.zprofile ~/.zprofile
+
+unlink ~/.config/nvim/init.vim
+ln -s ${PWD}/neovim/init.vim ~/.config/nvim/init.vim
