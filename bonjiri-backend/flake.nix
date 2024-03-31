93ae75fa1cb334d30309f7b23100c6909e4b77af
Mutsuha Asada <me@momee.mt>
2024-03-31 11:40:07 +0900

    backend: initial commit

---
+++ bonjiri-backend/flake.nix
@@ -0,0 +1,28 @@
+{
+  description = "bonjiri-backend";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/23.11";
+    flake-utils.url = "github:numtide/flake-utils";
+    flake-compat = {
+      url = "github:edolstra/flake-compat";
+      flake = false;
+    };
+  };
+
+  outputs = { nixpkgs, flake-utils, ... }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+        };
+      in {
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [
+            nil
+            stack
+          ];
+        };
+      }
+    );
+}
