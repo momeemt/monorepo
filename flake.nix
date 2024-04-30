19f5746191afb495fcc8eeb341aa7121d66bf5da
Mutsuha Asada <me@momee.mt>
2024-04-30 14:38:46 +0900

    init: hinoki

---
+++ flake.nix
@@ -0,0 +1,49 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (import rust-overlay)
+          ];
+        };
+        toolchain = pkgs.rust-bin.stable.latest.default;
+        rustPlatform = pkgs.makeRustPlatform {
+          rustc = toolchain;
+          cargo = toolchain;
+        };
+        name = "hinoki";
+      in {
+        packages.default = rustPlatform.buildRustPackage {
+          inherit name;
+          version = "0.0.1";
+          src = ./.;
+          cargoLock.lockFile = ./Cargo.lock;
+        };
+
+        apps.${system}.default = {
+          type = "app";
+          program = "${self.packages.${system}.${name}}/bin/${name}";
+        };
+
+        devShell = pkgs.mkShell {
+          buildInputs = with pkgs; [toolchain nil alejandra];
+        };
+      }
+    );
+}
