b43dd46b2cad63f337c2ca67bc8db931e1787316
Mutsuha Asada <me@momee.mt>
2024-06-05 04:14:37 +0000

    add: simple 7seg counter

--- core/kakacpu.sv
+++ core/kakacpu.sv
@@ -1,9 +1,47 @@
-module kakacpu(
-	input wire i0,
-	input wire i1,
-	output wire [1:0] q
+module kakacpu (
+    input logic CLOCK_50,
+	  input logic [3:0] KEY,
+    output logic [6:0] HEX0
 );
 
-	assign q[0] = i0;
-	assign q[1] = i1;
+logic [31:0] counter;
+logic [3:0] digit;
+
+function [6:0] digit_to_7seg;
+  input [3:0] digit;
+  case (digit)
+    4'd0: digit_to_7seg = 7'b1000000;
+    4'd1: digit_to_7seg = 7'b1111001;
+    4'd2: digit_to_7seg = 7'b0100100;
+    4'd3: digit_to_7seg = 7'b0110000;
+    4'd4: digit_to_7seg = 7'b0011001;
+    4'd5: digit_to_7seg = 7'b0010010;
+    4'd6: digit_to_7seg = 7'b0000010;
+    4'd7: digit_to_7seg = 7'b1111000;
+    4'd8: digit_to_7seg = 7'b0000000;
+    4'd9: digit_to_7seg = 7'b0010000;
+    default: digit_to_7seg = 7'b1111111;
+  endcase
+endfunction
+
+always_ff @(posedge CLOCK_50 or negedge KEY[0]) begin
+    if (!KEY[0]) begin
+        counter <= 0;
+        digit <= 0;
+    end else begin
+        counter <= counter + 1;
+        if (counter == 50_000_000) begin
+            counter <= 0;
+            if (digit == 9) begin
+                digit <= 0;
+            end else begin
+                digit <= digit + 1;
+            end
+        end
+    end
+end
+
+assign HEX0 = digit_to_7seg(digit);
+
 endmodule
+
