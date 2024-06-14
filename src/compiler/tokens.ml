918898d9d05ae7f9be353cac40499b0565e1f324
Mutsuha Asada <me@momee.mt>
2024-06-14 16:08:27 +0900

    add: four_arithmetic_ops tokenizer test

--- src/compiler/tokens.ml
+++ src/compiler/tokens.ml
@@ -16,8 +16,8 @@ type token =
   | RightBracket  (** The ']' operator *)
   | Arrow  (** The '->' operator *)
   | VerticalBar  (** The '|' operator *)
-  | Dot (** The '.' operator *)
-  | Comma (** The ',' operator *)
+  | Dot  (** The '.' operator *)
+  | Comma  (** The ',' operator *)
   | True  (** The `true` keyword *)
   | False  (** The `false` keyword *)
   | Function  (** The `fun` keyword *)
@@ -35,3 +35,43 @@ type token =
   | Identifier of string
   | EOF
   | Invalid
+
+let equal_token t1 t2 =
+  match (t1, t2) with
+  | EOF, EOF -> true
+  | Identifier i1, Identifier i2 -> i1 = i2
+  | Int n1, Int n2 -> n1 = n2
+  | Float f1, Float f2 -> f1 = f2
+  | String s1, String s2 -> s1 = s2
+  | Plus, Plus -> true
+  | Hyphen, Hyphen -> true
+  | Asterisk, Asterisk -> true
+  | Slash, Slash -> true
+  | Equal, Equal -> true
+  | Less, Less -> true
+  | Greater, Greater -> true
+  | NotEqual, NotEqual -> true
+  | Arrow, Arrow -> true
+  | SemiColon, SemiColon -> true
+  | DoubleColon, DoubleColon -> true
+  | Colon, Colon -> true
+  | Dot, Dot -> true
+  | Comma, Comma -> true
+  | LeftParen, LeftParen -> true
+  | RightParen, RightParen -> true
+  | LeftBracket, LeftBracket -> true
+  | RightBracket, RightBracket -> true
+  | VerticalBar, VerticalBar -> true
+  | True, True -> true
+  | False, False -> true
+  | Function, Function -> true
+  | Recursive, Recursive -> true
+  | Let, Let -> true
+  | In, In -> true
+  | If, If -> true
+  | Then, Then -> true
+  | Else, Else -> true
+  | Match, Match -> true
+  | With, With -> true
+  | Invalid, Invalid -> true
+  | _ -> false
--- test/test_tokenizer.ml
+++ test/test_tokenizer.ml
@@ -1,27 +1,75 @@
 open Compiler.Tokenizer
 open Compiler.Tokens
 
-let test_tokenize _ =
+let token_testable =
+  Alcotest.testable
+    (Fmt.of_to_string (fun t ->
+         match t with
+         | EOF -> "EOF"
+         | Identifier i -> "Identifier(" ^ i ^ ")"
+         | Int n -> "Int(" ^ string_of_int n ^ ")"
+         | Float f -> "Float(" ^ string_of_float f ^ ")"
+         | String s -> "String(" ^ s ^ ")"
+         | Plus -> "Plus"
+         | Hyphen -> "Hyphen"
+         | Asterisk -> "Asterisk"
+         | Slash -> "Slash"
+         | Equal -> "Equal"
+         | Less -> "Less"
+         | Greater -> "Greater"
+         | NotEqual -> "NotEqual"
+         | Arrow -> "Arrow"
+         | SemiColon -> "SemiColon"
+         | DoubleColon -> "DoubleColon"
+         | Colon -> "Colon"
+         | Dot -> "Dot"
+         | Comma -> "Comma"
+         | LeftParen -> "LeftParen"
+         | RightParen -> "RightParen"
+         | LeftBracket -> "LeftBracket"
+         | RightBracket -> "RightBracket"
+         | VerticalBar -> "VerticalBar"
+         | True -> "True"
+         | False -> "False"
+         | Function -> "Function"
+         | Recursive -> "Recursive"
+         | Let -> "Let"
+         | In -> "In"
+         | If -> "If"
+         | Then -> "Then"
+         | Else -> "Else"
+         | Match -> "Match"
+         | With -> "With"
+         | Invalid -> "Invalid"))
+    equal_token
+
+let test_tokenize_four_arithmetic_ops _ =
   let input = "a + b * 42 - (c / 3)" in
-  let _ = tokenize input in
-  let _ = [
-    Identifier "a";
-    Plus;
-    Identifier "b";
-    Asterisk;
-    Int 42;
-    Hyphen;
-    LeftParen;
-    Identifier "c";
-    Slash;
-    Int 3;
-    RightParen;
-    EOF;
-  ] in
-  Alcotest.fail "test"
+  let tokens = tokenize input in
+  let expect =
+    [
+      Identifier "a";
+      Plus;
+      Identifier "b";
+      Asterisk;
+      Int 42;
+      Hyphen;
+      LeftParen;
+      Identifier "c";
+      Slash;
+      Int 3;
+      RightParen;
+      EOF;
+    ]
+  in
+  Alcotest.(check (list token_testable)) "same tokens" tokens expect
 
 let () =
-  Alcotest.run "Wascaml.Compiler.tokenizer" [
-    ("tokenizer", [Alcotest.test_case "test_tokenize" `Quick test_tokenize; ])
-  ]
-
+  Alcotest.run "Wascaml.Compiler.tokenizer"
+    [
+      ( "tokenizer",
+        [
+          Alcotest.test_case "four_arithmetic_ops" `Quick
+            test_tokenize_four_arithmetic_ops;
+        ] );
+    ]
