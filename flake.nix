e85b21699ee8ad2e71deecb11a04db785daae6cb
Mutsuha Asada <me@momee.mt>
2024-07-03 10:39:04 +0900

    init: negima

---
+++ flake.nix
@@ -0,0 +1,57 @@
+{
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    rust-overlay = {
+      url = "github:oxalica/rust-overlay";
+      inputs.nixpkgs.follows = "nixpkgs";
+    };
+    flake-utils.url = "github:numtide/flake-utils";
+  };
+
+  outputs = {
+    self,
+    nixpkgs,
+    rust-overlay,
+    flake-utils,
+  }:
+    flake-utils.lib.eachDefaultSystem (
+      system: let
+        pkgs = import nixpkgs {
+          inherit system;
+          overlays = [
+            (import rust-overlay)
+          ];
+        };
+        toolchain = pkgs.rust-bin.stable.latest.default;
+        buildInputsForBuild = with pkgs;
+          [
+            openssl
+            openssl.dev
+          ]
+          ++ pkgs.lib.optional pkgs.stdenv.isDarwin [
+            darwin.Security
+            darwin.apple_sdk.frameworks.SystemConfiguration
+          ];
+        nativeBuildInputsForBuild = with pkgs; [pkg-config];
+        generatedBuild = pkgs.callPackage ./Cargo.nix {};
+      in rec {
+        devShells.default = pkgs.mkShell {
+          buildInputs = with pkgs;
+            buildInputsForBuild
+            ++ nativeBuildInputsForBuild
+            ++ [
+              alejandra
+              nil
+              toolchain
+              rust-analyzer
+            ];
+        };
+        packages.negima = generatedBuild.rootCrate.build;
+        packages.default = packages.negima;
+        apps.${system}.default = {
+          type = "app";
+          program = "${self.packages.default}/bin/negima";
+        };
+      }
+    );
+}
