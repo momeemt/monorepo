a734da1639a85722877d5b1e796049386c571cbf
Mutsuha Asada <me@momee.mt>
2024-10-30 02:06:28 +0000

    refactor: add reg_arith_kind_t enum

--- core/src/decode/r_type.sv
+++ core/src/decode/r_type.sv
@@ -1,3 +1,5 @@
+import opcode_type::*;
+
 // The `r_type` module receives three bits funct3 and seven bits funct7.
 // It outputs an instr kind, but it can only process RV32I instructions.
 module r_type (
@@ -5,57 +7,36 @@ module r_type (
     input logic rst,
     input logic [2:0] funct3,
     input logic [6:0] funct7,
-    output logic is_add,
-    output logic is_sub,
-    output logic is_sll,
-    output logic is_slt,
-    output logic is_sltu,
-    output logic is_xor,
-    output logic is_srl,
-    output logic is_sra,
-    output logic is_or,
-    output logic is_and
+    output reg_arith_kind_t kind
 );
   always @(posedge rst) begin
-    is_add  <= 0;
-    is_sub  <= 0;
-    is_sll  <= 0;
-    is_slt  <= 0;
-    is_sltu <= 0;
-    is_xor  <= 0;
-    is_srl  <= 0;
-    is_sra  <= 0;
-    is_or   <= 0;
-    is_and  <= 0;
+    kind <= rak_invalid;
   end
 
   always @(posedge clk or negedge rst) begin
-    is_add  <= 0;
-    is_sub  <= 0;
-    is_sll  <= 0;
-    is_slt  <= 0;
-    is_sltu <= 0;
-    is_xor  <= 0;
-    is_srl  <= 0;
-    is_sra  <= 0;
-    is_or   <= 0;
-    is_and  <= 0;
+    kind <= rak_invalid;
 
     case (funct3)
       3'b000: begin
-        is_add <= (funct7 == 7'b0000000);
-        is_sub <= (funct7 == 7'b0100000);
+        case (funct7)
+          7'b0000000: kind <= rak_add;
+          7'b0100000: kind <= rak_sub;
+          default: ;
+        endcase
       end
-      3'b001:  is_sll <= 1;
-      3'b010:  is_slt <= 1;
-      3'b011:  is_sltu <= 1;
-      3'b100:  is_xor <= 1;
+      3'b001:  kind <= rak_sll;
+      3'b010:  kind <= rak_slt;
+      3'b011:  kind <= rak_sltu;
+      3'b100:  kind <= rak_xor;
       3'b101: begin
-        is_srl <= (funct7 == 7'b0000000);
-        is_sra <= (funct7 == 7'b0100000);
+        case (funct7)
+          7'b0000000: kind <= rak_srl;
+          7'b0100000: kind <= rak_sra;
+          default: ;
+        endcase
       end
-      3'b110:  is_or <= 1;
-      3'b111:  is_and <= 1;
+      3'b110:  kind <= rak_or;
+      3'b111:  kind <= rak_and;
       default: ;
     endcase
   end
--- core/src/decode/test_bench/r_type_tb.sv
+++ core/src/decode/test_bench/r_type_tb.sv
@@ -1,14 +1,11 @@
 `timescale 1ns / 1ps
 
+import opcode_type::*;
+
 function automatic void display_error(input string testcase_name, input logic [2:0] funct3,
-                                      input logic [6:0] funct7, input logic is_add,
-                                      input logic is_sub, input logic is_sll, input logic is_slt,
-                                      input logic is_sltu, input logic is_xor, input logic is_srl,
-                                      input logic is_sra, input logic is_or, input logic is_and);
-  $error(
-      "decode::r_type - %s failed\nfunct3: %b | funct7: %b\nis_add: %b | is_sub: %b | is_sll: %b | is_slt: %b | is_sltu: %b | is_xor: %b | is_srl: %b | is_sra: %b | is_or: %b | is_and: %b",
-      testcase_name, funct3, funct7, is_add, is_sub, is_sll, is_slt, is_sltu, is_xor, is_srl,
-      is_sra, is_or, is_and);
+                                      input logic [6:0] funct7, input reg_arith_kind_t kind);
+  $error("decode::r_type - %s failed\nfunct3: %b | funct7: %b\nkind: %p", testcase_name, funct3,
+         funct7, kind);
 endfunction
 
 module r_type_tb;
@@ -16,32 +13,14 @@ module r_type_tb;
   logic rst;
   logic [2:0] funct3;
   logic [6:0] funct7;
-  logic is_add;
-  logic is_sub;
-  logic is_sll;
-  logic is_slt;
-  logic is_sltu;
-  logic is_xor;
-  logic is_srl;
-  logic is_sra;
-  logic is_or;
-  logic is_and;
+  reg_arith_kind_t kind;
 
   r_type uut (
       .clk(clk),
       .rst(rst),
       .funct3(funct3),
       .funct7(funct7),
-      .is_add(is_add),
-      .is_sub(is_sub),
-      .is_sll(is_sll),
-      .is_slt(is_slt),
-      .is_sltu(is_sltu),
-      .is_xor(is_xor),
-      .is_srl(is_srl),
-      .is_sra(is_sra),
-      .is_or(is_or),
-      .is_and(is_and)
+      .kind(kind)
   );
 
   always #5 clk = ~clk;
@@ -57,202 +36,72 @@ module r_type_tb;
     #10 funct3 = 3'b0;
     #10 funct7 = 7'b0;
     #10
-    assert(
-      is_add == 1 &&
-      is_sub == 0 &&
-      is_sll == 0 &&
-      is_slt == 0 &&
-      is_sltu == 0 &&
-      is_xor == 0 &&
-      is_srl == 0 &&
-      is_sra == 0 &&
-      is_or == 0 &&
-      is_and == 0
-      )
-    else
-      display_error("Testcase ADD failed", funct3, funct7, is_add, is_sub, is_sll, is_slt, is_sltu,
-                    is_xor, is_srl, is_sra, is_or, is_and);
+    assert (kind == rak_add)
+    else display_error("Testcase ADD failed", funct3, funct7, kind);
 
     // SUB
     #10 funct3 = 3'b0;
     #10 funct7 = 7'b0100000;
     #10
-    assert (
-      is_add == 0 &&
-      is_sub == 1 &&
-      is_sll == 0 &&
-      is_slt == 0 &&
-      is_sltu == 0 &&
-      is_xor == 0 &&
-      is_srl == 0 &&
-      is_sra == 0 &&
-      is_or == 0 &&
-      is_and == 0
-    )
-    else
-      display_error("Testcase SUB failed", funct3, funct7, is_add, is_sub, is_sll, is_slt, is_sltu,
-                    is_xor, is_srl, is_sra, is_or, is_and);
+    assert (kind == rak_sub)
+    else display_error("Testcase SUB failed", funct3, funct7, kind);
 
     // SLL
     #10 funct3 = 3'b001;
     #10 funct7 = 7'b0;
     #10
-    assert (
-      is_add == 0 &&
-      is_sub == 0 &&
-      is_sll == 1 &&
-      is_slt == 0 &&
-      is_sltu == 0 &&
-      is_xor == 0 &&
-      is_srl == 0 &&
-      is_sra == 0 &&
-      is_or == 0 &&
-      is_and == 0
-      )
-    else
-      display_error("Testcase SLL failed", funct3, funct7, is_add, is_sub, is_sll, is_slt, is_sltu,
-                    is_xor, is_srl, is_sra, is_or, is_and);
+    assert (kind == rak_sll)
+    else display_error("Testcase SLL failed", funct3, funct7, kind);
 
     // SLT
     #10 funct3 = 3'b010;
     #10 funct7 = 7'b0;
     #10
-    assert (
-      is_add == 0 &&
-      is_sub == 0 &&
-      is_sll == 0 &&
-      is_slt == 1 &&
-      is_sltu == 0 &&
-      is_xor == 0 &&
-      is_srl == 0 &&
-      is_sra == 0 &&
-      is_or == 0 &&
-      is_and == 0
-      )
-    else
-      display_error("Testcase SLT failed", funct3, funct7, is_add, is_sub, is_sll, is_slt, is_sltu,
-                    is_xor, is_srl, is_sra, is_or, is_and);
+    assert (kind == rak_slt)
+    else display_error("Testcase SLT failed", funct3, funct7, kind);
 
     // SLTU
     #10 funct3 = 3'b011;
     #10 funct7 = 7'b0;
     #10
-    assert (
-      is_add == 0 &&
-      is_sub == 0 &&
-      is_sll == 0 &&
-      is_slt == 0 &&
-      is_sltu == 1 &&
-      is_xor == 0 &&
-      is_srl == 0 &&
-      is_sra == 0 &&
-      is_or == 0 &&
-      is_and == 0
-      )
-    else
-      display_error("Testcase SLTU failed", funct3, funct7, is_add, is_sub, is_sll, is_slt, is_sltu,
-                    is_xor, is_srl, is_sra, is_or, is_and);
+    assert (kind == rak_sltu)
+    else display_error("Testcase SLTU failed", funct3, funct7, kind);
 
     // XOR
     #10 funct3 = 3'b100;
     #10 funct7 = 7'b0;
     #10
-    assert (
-      is_add == 0 &&
-      is_sub == 0 &&
-      is_sll == 0 &&
-      is_slt == 0 &&
-      is_sltu == 0 &&
-      is_xor == 1 &&
-      is_srl == 0 &&
-      is_sra == 0 &&
-      is_or == 0 &&
-      is_and == 0
-      )
-    else
-      display_error("Testcase XOR failed", funct3, funct7, is_add, is_sub, is_sll, is_slt, is_sltu,
-                    is_xor, is_srl, is_sra, is_or, is_and);
+    assert (kind == rak_xor)
+    else display_error("Testcase XOR failed", funct3, funct7, kind);
 
     // SRL
     #10 funct3 = 3'b101;
     #10 funct7 = 7'b0;
     #10
-    assert (
-      is_add == 0 &&
-      is_sub == 0 &&
-      is_sll == 0 &&
-      is_slt == 0 &&
-      is_sltu == 0 &&
-      is_xor == 0 &&
-      is_srl == 1 &&
-      is_sra == 0 &&
-      is_or == 0 &&
-      is_and == 0
-      )
-    else
-      display_error("Testcase SRL failed", funct3, funct7, is_add, is_sub, is_sll, is_slt, is_sltu,
-                    is_xor, is_srl, is_sra, is_or, is_and);
+    assert (kind == rak_srl)
+    else display_error("Testcase SRL failed", funct3, funct7, kind);
 
     $finish;
     // SRA
     #10 funct3 = 3'b101;
     #10 funct7 = 7'b0100000;
     #10
-    assert (
-      is_add == 0 &&
-      is_sub == 0 &&
-      is_sll == 0 &&
-      is_slt == 0 &&
-      is_sltu == 0 &&
-      is_xor == 0 &&
-      is_srl == 0 &&
-      is_sra == 1 &&
-      is_or == 0 &&
-      is_and == 0
-      )
-    else
-      display_error("Testcase SRA failed", funct3, funct7, is_add, is_sub, is_sll, is_slt, is_sltu,
-                    is_xor, is_srl, is_sra, is_or, is_and);
+    assert (kind == rak_sra)
+    else display_error("Testcase SRA failed", funct3, funct7, kind);
 
     // OR
     #10 funct3 = 3'b110;
     #10 funct7 = 7'b0;
     #10
-    assert (
-      is_add == 0 &&
-      is_sub == 0 &&
-      is_sll == 0 &&
-      is_slt == 0 &&
-      is_sltu == 0 &&
-      is_xor == 0 &&
-      is_srl == 0 &&
-      is_sra == 0 &&
-      is_or == 1 &&
-      is_and == 0
-      )
-    else
-      display_error("Testcase OR failed", funct3, funct7, is_add, is_sub, is_sll, is_slt, is_sltu,
-                    is_xor, is_srl, is_sra, is_or, is_and);
+    assert (kind == rak_or)
+    else display_error("Testcase OR failed", funct3, funct7, kind);
 
     // AND
     #10 funct3 = 3'b111;
     #10 funct7 = 7'b0;
     #10
-    assert (
-      is_add == 0 &&
-      is_sub == 0 &&
-      is_sll == 0 &&
-      is_slt == 0 &&
-      is_sltu == 0 &&
-      is_xor == 0 &&
-      is_srl == 0 &&
-      is_sra == 0 &&
-      is_or == 0 &&
-      is_and == 1
-      )
-    else
-      display_error("Testcase AND failed", funct3, funct7, is_add, is_sub, is_sll, is_slt, is_sltu,
-                    is_xor, is_srl, is_sra, is_or, is_and);
+    assert (kind == rak_and)
+    else display_error("Testcase AND failed", funct3, funct7, kind);
 
     $finish;
   end
